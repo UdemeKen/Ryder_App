5211eae9a1266d076e18ddfb01c792b8015bd317
Your Name~ <you@example.com>
2023-12-19 09:32:43 +0100

    Initial commit

---
+++ Ryder_Backend/src/main/java/com/javacluster/Ryder/RyderApplication.java
@@ -0,0 +1,13 @@
+package com.javacluster.Ryder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyderApplication.class, args);
+	}
+
+}
---
+++ Ryder_Backend/src/test/java/com/javacluster/Ryder/RyderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacluster.Ryder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
